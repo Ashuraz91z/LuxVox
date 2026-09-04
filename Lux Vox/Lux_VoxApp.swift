937469bd9decfed96bd42d2cc0bd027ad474c718
Lucas Fernandes <admin@lux-audere.com>
2026-09-04 11:11:50 +0200

    Initial Commit

---
+++ Lux Vox/Lux_VoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lux_VoxApp.swift
+//  Lux Vox
+//
+//  Created by Lucas Fernandes on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Lux_VoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
